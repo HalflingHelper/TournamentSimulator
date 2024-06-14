2e3fa5b133ebec24c923da7c00efdaf8d754a099
halflinghelper <calvinjosenhans10@gmail.com>
2024-06-14 14:34:53 -0400

    r8

--- js/madrid.js
+++ js/madrid.js
@@ -77,11 +77,11 @@ const rounds = [
         new Game(players[6], players[4]).setResult(GameResults.BLACK),
     ]),
     new Round([
-        new Game(players[1], players[4]),
-        new Game(players[7], players[6]),
-        new Game(players[5], players[3]),
-        new Game(players[2], players[9]),
-        new Game(players[8], players[0]),
+        new Game(players[1], players[4]).setResult(GameResults.WHITE),
+        new Game(players[7], players[6]).setResult(GameResults.WHITE),
+        new Game(players[5], players[3]).setResult(GameResults.DRAW),
+        new Game(players[2], players[9]).setResult(GameResults.WHITE),
+        new Game(players[8], players[0]).setResult(GameResults.BLACK),
     ]),
     new Round([
         new Game(players[0], players[1]),
